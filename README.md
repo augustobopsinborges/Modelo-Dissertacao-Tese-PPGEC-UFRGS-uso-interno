ceaac1046f8a19f51254b83bd0e173baa0f9bcec
augusto <augustobopsinborges@gmail.com>
2021-02-08 21:38:52 -0300

    atualização2021
    com correções e acréscimos de Felipe Quevedo e Rodrigo Pereira.

--- README.md
+++
@@ -1,4 +0,0 @@
-## Modelo padrão para Dissertações e Teses PPGEC-UFRGS (uso interno)
-
-**Arquivo principal**: *main.tex*
-
---
+++ limpar.bat
@@ -0,0 +1,26 @@
+cls
+echo "Limpando arquivos..."
+del *.loq
+del /S *.aux
+del *.bbl 
+del *.blg 
+del *.brf 
+del *.dvi 
+del *.ilg 
+del *.ind 
+del /S *.log 
+del *.out 
+del *.nav 
+del *.snm 
+del *.toc 
+del *.idx 
+del *.lof 
+del *.lot 
+del *.snm 
+del *.bcf 
+del *.run.xml 
+del *.synctex.gz 
+del main.pdf
+del *.vrb,
+cls
+echo "Terminado!"
